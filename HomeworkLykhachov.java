f8afe63be3f7258a33a68fc519d391a709e67f17
Liaso1 <liasol97@gmail.com>
2023-08-06 22:01:57 -0400

    second message

--- HomeworkLykhachov.java
+++ HomeworkLykhachov.java
@@ -1,5 +1,6 @@
 public class HomeworkLykhachov {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world from Kyrylo");
     }
 }
